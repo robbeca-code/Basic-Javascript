b6234e69297c231bbc2e2ccc6c6e534e11d8a6dc
robbeca <xmfkddl1006@gmail.com>
2022-10-29 22:19:32 +0900

    Add filter buttons

--- menu/app.js
+++ menu/app.js
@@ -73,29 +73,77 @@ const menu = [
   },
 ];
 
-const sectionCenter = document.querySelector(".section-center");
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('.btn-container');
 
-window.addEventListener("DOMContentLoaded", function () {
-	displayMenu();
+window.addEventListener('DOMContentLoaded', function () {
+  displayMenuItems(menu);
+  displayMenuButtons();
 });
 
-function displayMenu() {
-	let displayMenu = menu.map(function(item){
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function(item){
 
-		return `<article class="menu-container">
-		<img src=${item.img} class="menu-img" alt=${item.title}/>
-		<div class="menu-info">
-			<header class="menu-header">
-				<h4 class="menu-title">${item.title}</h4>
-				<h4 class="menu-price">${item.price}</h4>
-			</header>
-			<p class="menu-description">
-				${item.desc}
-			</p>
-		</div>
-	</article>`;
-	});
+  return `<article class="menu-container">
+    <img src=${item.img} class="menu-img" alt=${item.title}/>
+    <div class="menu-info">
+    <header class="menu-header">
+    <h4 class="menu-title">${item.title}</h4>
+    <h4 class="menu-price">${item.price}</h4>
+    </header>
+    <p class="menu-description">
+    ${item.desc}
+    </p>
+    </div>
+    </article>`;
+});
 
-	displayMenu = displayMenu.join("");
-	sectionCenter.innerHTML = displayMenu;
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+}
+
+/*
+  reduce((누적값, 현재값) => {return 결과}, 초기값);
+  누적값에 들어있는 값
+  [all]
+  [all, breakfast]
+  [all, breakfast, lunch]
+  [all, breakfast, lunch, shakes]
+  
+  현재값에 들어있는 값은 menu.reduce 했을 때 menu의 내용이 들어가있다.
+*/
+
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if(!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    }, ['all']
+  );
+
+  const categoryBtns = categories.map(function(category){
+    return `<button type="button" class="filter-btn" data-id=${category}>
+      ${category}</button>`;
+  }).join("");
+
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll('.filter-btn');
+
+  filterBtns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function(menuItem){
+        if(menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if(category === 'all') {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
 }
