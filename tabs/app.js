fdf69bc3275bbd679f358965842d6ef9761ff0fb
robbeca <xmfkddl1006@gmail.com>
2022-11-02 22:39:39 +0900

    Delete a remark

--- tabs/app.js
+++ tabs/app.js
@@ -16,14 +16,3 @@ about.addEventListener('click', function(e){
     element.classList.add('active');
   });
 });
-
-// 이렇게 하면 click한 tab에 있는 active를 삭제하고
-// 현재 선택한 객체에 active가 들어가는 코드라서 바뀌는게 없다.
-// tabs.forEach(function(tab){
-//   tab.addEventListener('click', function(e){
-//     const id=e.target.dataset.id;
-//     console.log(tab);
-//     tab.classList.remove('active');
-//     e.target.classList.add('active');
-//   });
-// });
